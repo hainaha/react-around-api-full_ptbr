5164aaa1a55fd4e0b21987f9efa1bc44e136a057
hainaha <hainaha@gmail.com>
2023-02-12 18:56:20 -0300

    Cria arquivo .env no backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,24 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorized-err');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Autorização necessária' });
+    throw new UnauthorizedError(err.message);
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'BF7DC92FA0DCB24F');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'BF7DC92FA0DCB24F'
+    );
   } catch (err) {
-    return res.status(401).send({ message: 'Autorização necessária' });
+    throw new UnauthorizedError(err.message);
   }
 
   req.user = payload;
