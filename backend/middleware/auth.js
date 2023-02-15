28041f722dc55efbfdfc7ec42abb5275e7e10200
hainaha <hainaha@gmail.com>
2023-02-14 22:38:20 -0300

    Revert "Cria arquivo .env no backend"
    This reverts commit 5164aaa1a55fd4e0b21987f9efa1bc44e136a057.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,19 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/unauthorized-err');
-const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError(err.message);
+    return res.status(401).send({ message: 'Autorização necessária' });
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'BF7DC92FA0DCB24F'
-    );
+    payload = jwt.verify(token, 'BF7DC92FA0DCB24F');
   } catch (err) {
-    throw new UnauthorizedError(err.message);
+    return res.status(401).send({ message: 'Autorização necessária' });
   }
 
   req.user = payload;
